2ade57b6f648a76bebc5c735ec2f0f62db1900d6
jpenanje <jpenanje@gmail.com>
2022-03-10 22:20:56 +0000

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,8 +7,10 @@
  */
 int main(void)
 {
-char *str="and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-while(*str)
+char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+while (*str)
+{
   putchar(*str++);
+}
 return (1);
 }

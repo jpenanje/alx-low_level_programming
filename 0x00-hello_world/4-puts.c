756a740e719bce23896aafeb28aa72f4a34e478d
jpenanje <jpenanje@gmail.com>
2022-03-10 19:35:26 +0000

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,9 +6,7 @@
  */
 int main(void)
 {
-  char message[] = "\"Programming is like building a multilingual puzzle\n";
-
-  puts(message);
-  
-  return (0);
+char message[] = "\"Programming is like building a multilingual puzzle\n";
+puts(message);
+return (0);
 }

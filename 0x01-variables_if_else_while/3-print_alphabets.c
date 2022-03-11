94817f3bbf612179978ccd20251b443f5557c0cd
jpenanje <jpenanje@gmail.com>
2022-03-11 22:24:15 +0000

    task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Print the letters of alphabet in lower and upper case.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char ltr, L;
+
+ltr = 'a';
+L = 'A'
+while (ltr <= 'z')
+{
+putchar(ltr);
+ltr++;
+}
+while (L <= 'Z')
+{
+putchar(L);
+L++;
+}
+putchar('\n');
+return (0);
+}

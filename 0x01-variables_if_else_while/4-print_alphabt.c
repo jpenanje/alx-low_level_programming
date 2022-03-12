818c6ecf35bad67a3e3debf14ba6e6faca528bcf
jpenanje <99893508+jpenanje@users.noreply.github.com>
2022-03-12 02:06:43 +0100

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -6,16 +6,15 @@
  */
 int main(void)
 {
-        char ltr;  
-  
-        for (ltr = 'a'; ltr <= 'z'; ltr++)
-        {
-                if (ltr == 'e' || ltr == 'q')
-                {
-                        continue;
-                }
-                putchar(ltr);
-        }
-        putchar('\n');
-        return (0);
+char ltr;  
+for (ltr = 'a'; ltr <= 'z'; ltr++)
+{
+if (ltr == 'e' || ltr == 'q')
+{
+continue;
+}
+putchar(ltr);
+}
+putchar('\n');
+return (0);
 }

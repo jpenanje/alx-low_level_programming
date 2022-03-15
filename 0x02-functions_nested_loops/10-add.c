eda1198f09e712dbcc6502c7d5b843d99cd0307e
jpenanje <jpenanje@gmail.com>
2022-03-15 23:20:12 +0000

    task 10, addition

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - add two numbers and return the sum
+ * @n1,@n2: Numbers to be added
+ *
+ * Return: Always 0 (Success)
+ */
+
+int add(int n1, int n2)
+{
+return (n1 + n2);
+}

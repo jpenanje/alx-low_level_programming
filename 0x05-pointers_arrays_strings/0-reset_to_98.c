3a088766a496195dca437ffec80e8ca45d7eb9fc
jpenanje <jpenanje@gmail.com>
2022-03-21 16:34:15 +0000

    update the value of a pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - pointer as parameter and updates its value
+ * @n: pointer to be updated
+ *
+ * Return: Always 0 (Success)
+ */
+
+void reset_to_98(int *n)
+{
+*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+POINTERS, ARRAYS AND STRINGS
+Task-0: Function that takes a pointer as param and updates its value

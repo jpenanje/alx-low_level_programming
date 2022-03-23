e686930b6a57184d44af36ab2faa6530c8486ecd
jpenanje <jpenanje@gmail.com>
2022-03-23 09:20:37 +0000

    reverse array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverse array content
+ * @a: pointer to the array
+ * @n: number of elements
+ *
+ * Return nothing
+ */
+
+void reverse_array(int *a, int n)
+{
+int i;
+
+i = (n - 1);
+while (i >= 0)
+{
+if (i == 0)
+{
+_putchar(a[i]);
+}
+else
+{
+_putchar(a[i]);
+_putchar(',');
+_putchar(' ');
+}
+i--;
+}
+}

a9cf320681254fbfc992f2c35a746e3c1441c1b1
jpenanje <99893508+jpenanje@users.noreply.github.com>
2022-03-25 00:09:07 +0100

    Update 7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -9,24 +9,24 @@
 
 char *leet(char *s)
 {
-  int count, i;
-  int low_letters[] = {97, 101, 111, 116, 108};
-  int upp_letters[] = {65, 69, 79, 84, 76};
-  int numbers[] = {52, 51, 48, 55, 49};
+int count, i;
+int low_letters[] = {97, 101, 111, 116, 108};
+int upp_letters[] = {65, 69, 79, 84, 76};
+int numbers[] = {52, 51, 48, 55, 49};
 
-  count = 0;
-  while (*(s + count) != '\0')
-    {
-      for (i = 0; i < 5; i++)
-	{
-	  if (*(s + count) == low_letters[i] || *(s + count) == upp_letters[i])
-	    {
-	      *(s + count) = numbers[i];
-	      break;
-	    }
-	}
-      count++;
-    }
+count = 0;
+while (*(s + count) != '\0')
+{
+for (i = 0; i < 5; i++)
+{
+if (*(s + count) == low_letters[i] || *(s + count) == upp_letters[i])
+{
+*(s + count) = numbers[i];
+break;
+}
+}
+count++;
+}
 
-  return (s);
+return (s);
 }

7b4ef273b4a7f17c3b42239e7eca28a85cffcbf9
jpenanje <99893508+jpenanje@users.noreply.github.com>
2022-03-25 00:12:17 +0100

    Update 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -12,7 +12,9 @@ int sep_words[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
 count = 0;
 if (*(s + count) >= 97 && *(s + count) <= 122)
+{
 *(s + count) = *(s + count) - 32;
+}
 count++;
 while (*(s + count) != '\0')
 {

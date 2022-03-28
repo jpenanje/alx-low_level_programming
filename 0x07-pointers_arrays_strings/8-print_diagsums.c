a398ff24bd31cacf71c52a044bb2efd35b431fff
jpenanje <jpenanje@gmail.com>
2022-03-28 21:30:21 +0000

    sum diagonal

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_diagsums - sum of diagonal
+ * @a: pointer
+ * @size: array size
+ *
+ * Return: nothing
+ */
+
+void print_diagsums(int *a, int size)
+{
+int i, j, sum;
+
+sum = 0;
+for (i = 0; i < size; i++)
+{
+for (j = 0; j < size; j++)
+{
+if (i == j)
+{
+sum += a[i][j];
+}
+}
+}
+}

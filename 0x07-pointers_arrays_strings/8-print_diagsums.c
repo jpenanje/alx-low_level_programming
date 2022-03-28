4e608331561842ad869c55c241852fdef0c7ce86
jpenanje <99893508+jpenanje@users.noreply.github.com>
2022-03-28 22:56:22 +0100

    Update 8-print_diagsums.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -15,9 +15,13 @@ int i, sum1 = 0, sum2 = 0;
 for (i = 0; i < (size * size); i++)
 {
 if (i % (size + 1) == 0)
-  sum1 += *(a + i);
+{
+sum1 += *(a + i);
+}
 if (i % (size - 1) == 0 && i != 0 && i < size * size - 1)
-  sum2 += *(a + i);
+{
+sum2 += *(a + i);
+}
 }
 printf("%d, %d\n", sum1, sum2);
 }

53dc5e01881c5d33f51d9710a6ee85b736aa42bc
jpenanje <jpenanje@gmail.com>
2022-03-28 19:31:24 +0000

    memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _memcpy - copies n bytes
+ * @dest: destination
+ * @src: source
+ * @n: n bytes
+ *
+ * Return: pointer to dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n);
+{
+return (memcpy(dest, src, n));
+}

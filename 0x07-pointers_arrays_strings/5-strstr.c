3a223a270d9cf3f594d6dd4dbd723f7379b0e268
jpenanje <jpenanje@gmail.com>
2022-03-28 20:19:55 +0000

    task 3 - 5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strstr - pointer
+ * @haystack: pointer
+ * @needle: substring
+ *
+ * Return: pointer to the beginning
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+return (strstr(haystack, needle));
+}

da99d221013be86313e1c040da227fca38d8ab4c
jpenanje <jpenanje@gmail.com>
2022-03-30 21:51:22 +0000

    square root

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -2,13 +2,35 @@
 #include <math.h>
 
 /**
+ * _sqrt - _sqrt_recursion
+ * @n: integer paramtr
+ * @i: integer parameter
+ * Return: sqrt
+ */
+int _sqrt(int n, int i)
+{
+if (n < 0)
+{
+return (-1);
+}
+if ((i * i) > n)
+{
+return (-1);
+}
+if (i * i == n)
+{
+return (i);
+}
+return (_sqrt(n, i + 1));
+}
+
+/**
  * _sqrt_recursion - calculates square root
  * @n: number
- * Return: square root
+ * Return: int
  */
 
 int _sqrt_recursion(int n)
 {
-  
-  return (sqrt(n));
+  return (_sqrt(n, 1));
 }

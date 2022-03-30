31a10a0b957123e0b168279f48066356e38f8f39
jpenanje <jpenanje@gmail.com>
2022-03-30 16:47:20 +0000

    5 and 6

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <math.h>
+
+/**
+ * _sqrt_recursion - calculates square root
+ * @n: number
+ * Return: square root
+ */
+
+int _sqrt_recursion(int n)
+{
+  
+  return (sqrt(n));
+}
---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * is_prime_number - prime number or not
+ * @n: number
+ * Return: 1 if prime number else 0
+ */
+
+int is_prime_number(int n)
+{
+if ((n % 2) != 0)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}

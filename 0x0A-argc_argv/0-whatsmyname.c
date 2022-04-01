c915cb721cb6de27e5d9791017bef952eec22ed6
jpenanje <jpenanje@gmail.com>
2022-04-01 09:24:38 +0000

    program name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - main function
+ * @argc - number of arguments
+ * @argv - array of arguments
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char **argv)
+{
+printf("%s\n", argv[0]);
+}

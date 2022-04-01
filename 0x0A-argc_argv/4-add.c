e392f0b71a7e175da73e0c7465c932f487116991
jpenanje <jpenanje@gmail.com>
2022-04-01 10:37:45 +0000

    add

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - add positive numbers
+ * @argc: number of cmd arguments
+ * @argv: array of cmd argument
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+int result, i;
+
+result = 0;
+if (argc == 1)
+{
+printf("0\n");
+}
+for (i = 1; i < argc; i++)
+{
+if (!isdigit(atoi(argv[i])))
+{
+printf("Error\n");
+return (1);
+break;
+}
+result += atoi(argv[i]);
+}
+printf("%d\n", result);
+return (0);
+}

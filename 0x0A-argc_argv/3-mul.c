f286a331968ee10905fb6dd308a362e91865319e
jpenanje <99893508+jpenanje@users.noreply.github.com>
2022-04-01 11:24:38 +0100

    Update 3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 /**
  * main - multiplies two numbers
  * @argc: number of cmd arguments
@@ -10,14 +11,14 @@ int main(int argc, char *argv[])
 {
 int result;
 
-if (argc < 2)
+if (argc < 3)
 {
 printf("Error\n");
 return (1);
 }
 else
 {
-result = argv[1] * argv[2];
+result = atoi(argv[1]) * atoi(argv[2]);
 printf("%d\n", result);
 }
 return (0);

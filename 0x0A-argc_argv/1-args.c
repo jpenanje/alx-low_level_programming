f5773ceb949baf509a348ebcd7f0501f6b728d28
jpenanje <jpenanje@gmail.com>
2022-04-01 09:46:39 +0000

    argument number

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - print number of arguments
+ * @argc: number of cmd arguments
+ * @argv: array of cmd arguments
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+printf("%d\n", argc);
+return (0);
+}

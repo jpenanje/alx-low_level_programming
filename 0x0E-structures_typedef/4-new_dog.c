d494e59609fff3c8092489397eda476a6d8b42fc
jpenanje <jpenanje@gmail.com>
2022-04-12 02:14:45 +0000

    new

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,25 @@
+#include "dog.h"
+
+/**
+ * new_dog - creates new dog
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ *
+ * Return: pointer to new dog
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+dog_t *new;
+  
+new = malloc(sizeof(dog_t));
+if (new == NULL)
+{
+return (NULL);
+}
+new->name = name;
+new->age = age;
+new->owner = owner;
+return (new);
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -24,4 +24,6 @@ void print_dog(struct dog *d);
  * dog_t - typedef for struct dog
  */
 typedef struct dog dog_t;
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 #endif

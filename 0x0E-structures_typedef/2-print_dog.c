99cac2e7d79ff7ddd27bca4f80d9de8c8c95ac5f
jpenanje <99893508+jpenanje@users.noreply.github.com>
2022-04-12 02:49:01 +0100

    Update 2-print_dog.c

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,7 +12,7 @@ void print_dog(struct dog *d)
 {
 if (d)
 {
-if (d->name == NULL)
+if (!(d->name))
 {
 printf("Name: (nil)\n");
 }
@@ -21,7 +21,7 @@ else
 printf("Name: %s\n", d->name);
 }
 print("Age: %f\n", d->age);
-if (d->owner == NULL)
+if (!(d->owner))
 {
 printf("Owner: (nil)\n");
 }

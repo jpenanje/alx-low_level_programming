d9ee8be6c3c11dd70e1a5ccea6cc6d2cae88d9dc
jpenanje <jpenanje@gmail.com>
2022-04-12 00:53:26 +0000

    struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef _DOG_H_
+#define _DOH_H_
+
+/**
+ * struct dog - dog details
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ */
+
+struct dog{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif

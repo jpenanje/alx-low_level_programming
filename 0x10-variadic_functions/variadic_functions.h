bab6fcd22d81e77e5178e8a81f672a85c253f1d2
jpenanje <99893508+jpenanje@users.noreply.github.com>
2022-04-14 15:16:59 +0100

    Update variadic_functions.h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,12 +1,13 @@
 #ifndef _VARIADIC_FUNCTIONS_H_
 #define _VARIADIC_FUNCTIONS_H_
 
-#include <stdarg.h>
-#include <stdio.h>
-
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
 #endif
+#include <stdarg.h>
+5
+#include <stdio.h>
+6

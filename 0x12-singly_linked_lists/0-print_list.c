0134afe7e1a7752bb541a1bf9efc2817ff361aa1
jpenanje <jpenanje@gmail.com>
2022-04-21 14:03:54 +0100

    print elements

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * print_list - print elements of list_t
+ * @h: list
+ * Return: number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+size_t nnodes;
+
+nnodes = 0;
+while (h != NULL)
+{
+if (h->str == NULL)
+{
+printf("[0] (nil)");
+}
+else
+{
+printf("[%d] %s", h->len, h->str);
+}
+h = h->next;
+nnodes++;
+}
+return (nnodes);
+}

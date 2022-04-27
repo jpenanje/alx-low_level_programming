0fb1deb5eb4c1c4c49fedb3b47f3babffcd49741
jpenanje <99893508+jpenanje@users.noreply.github.com>
2022-04-27 16:34:54 +0100

    Update lists.h

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,7 +1,5 @@
 #ifndef _LISTS_H_
 #define _LISTS_H_
-
-#include <stdio.h>
 #include <stdlib.h>
 
 /**
@@ -10,14 +8,13 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * 
  */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 
-char _putchar(char s);
+int _putchar(char c);
 size_t print_listint(const listint_t *h);
 #endif

cc6f0e94cd707906ef30f576576d0fb99057c9d2
jpenanje <99893508+jpenanje@users.noreply.github.com>
2022-04-27 15:52:17 +0100

    Update 0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -6,7 +6,6 @@
  * @h: list
  * Return: number of nodes
  */
-
 size_t print_listint(const listint_t *h)
 {
 int nnodes;
@@ -18,6 +17,5 @@ printf("%i\n", h->n);
 h = h->next;
 nnodes++;
 }
-
 return (nnodes);
 }

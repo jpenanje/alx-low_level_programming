63a36a1064a2578ff28deec4fd9128f06faf99ce
jpenanje <99893508+jpenanje@users.noreply.github.com>
2022-04-27 16:26:07 +0100

    Update 0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,19 +1,23 @@
+#include <stdio.h>
 #include "lists.h"
 
 /**
- * print_listint - prints all the elements of a list.
- * @h: head of a list.
+ * print_listint - Prints all the elements
+ * @h: Pointer to an int constant
  *
- * Return: numbers of nodes.
- */
+ * Return: The number of mine
+**/
+
 size_t print_listint(const listint_t *h)
 {
-size_t nnodes = 0;
-while (h != NULL)
-{
-printf("%d\n", h->n);
-h = h->next;
-nnodes++;
-}
-return (nnodes);
+unsigned int mine = 0;
+
+	while (h != NULL)
+	{
+		printf("%i\n", h->n);
+		h = h->next;
+		mine++;
+	}
+
+return (mine);
 }

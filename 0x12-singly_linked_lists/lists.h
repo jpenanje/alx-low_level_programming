a330a03296a8395b85b43dbf2283d0c0207fb1d0
jpenanje <99893508+jpenanje@users.noreply.github.com>
2022-04-27 15:09:39 +0100

    Update lists.h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -12,13 +12,12 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * for Holberton project
  */
 typedef struct list_s
 {
-	char *str;
-	unsigned int len;
-	struct list_s *next;
+    char *str;
+    unsigned int len;
+    struct list_s *next;
 } list_t;
 
 char _putchar(char s);

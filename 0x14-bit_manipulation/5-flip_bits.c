d803372b496fc7481d4ff5c66beb2daaca8ffbad
jpenanje <99893508+jpenanje@users.noreply.github.com>
2022-04-29 00:45:18 +0100

    Update 5-flip_bits.c

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
-* flip_bits - is a function set bit to 0 at given index.
-* @n: is unsigned long int
-* @m: is an unsigned int
-*
-* Return: return 1 when bit is 1 else 0.
-**/
+ * flip_bits - is a function set bit to 0 at given index.
+ * @n: is unsigned long int
+ * @m: is an unsigned int
+ *
+ * Return: return 1 when bit is 1 else 0.
+ */
 
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {

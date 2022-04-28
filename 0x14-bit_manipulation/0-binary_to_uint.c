886476b6899d5a9b0dcceb9f22cb775d5984dd96
jpenanje <jpenanje@gmail.com>
2022-04-28 22:09:06 +0000

    print binary

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,22 +9,22 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-int i;
-unsigned int n = 0;
+	int i;
+	unsigned int n = 0;
 
-if (!b)
-return (0);
+	if (!b)
+		return (0);
 
-for (i = 0; b[i] != '\0'; i++)
-{
-n <<= 1;
-if (b[i] == '1')
-n += 1;
-else
-{
-if (b[i] != '0' && b[i] != '1')
-return (0);
-}
-}
-return (n);
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		n <<= 1;
+		if (b[i] == '1')
+			n += 1;
+		else
+		{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+		}
+	}
+	return (n);
 }

da11e2b09eba63a9880357e3bc3b31b9ebee270e
jpenanje <99893508+jpenanje@users.noreply.github.com>
2022-05-05 18:07:32 +0100

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,4 +1,10 @@
 #include "main.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/uio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
 
 /**
  * read_textfile - read and print a text file to POSIX

3820f969f6fa975e67e43f9218b3b113fefddc93
jpenanje <jpenanje@gmail.com>
2022-05-05 16:55:20 +0000

    read and print text file

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * read_textfile - read and print a text file to POSIX
+ * @filename: file name
+ * @letters: number of letters
+ *
+ * Return: number of letters to read and print
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd, nread, nprnt;
+	char *buf;
+
+	if (filename == NULL)
+		return (0);
+
+	buf = malloc(sizeof(char) * (letters));
+	if (buf == NULL)
+		return (0);
+
+	fd = open(filename, 0_RDONLY);
+	if (fd == -1)
+		return (0);
+
+	nread = read(fd, buf, letters);
+	if (nread < 0)
+		return (0);
+
+	nprnt = write(STDOUT_FILENO, buf, nread);
+	if (nprnt < 0)
+		return (0);
+
+	free(buf);
+	close(fd);
+	return (nprint);
+}
+
---
+++ 0x15-file_io/main.h
@@ -0,0 +1,13 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/uio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+
+ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif

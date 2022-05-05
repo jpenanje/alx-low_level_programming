21e0795beb8c6e1e9ed2e94b94d40afb3b974967
jpenanje <jpenanje@gmail.com>
2022-05-05 17:36:58 +0000

    updte create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,11 +21,11 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, 0_CREAT | 0_RDWR | 0_TRUNC, 0600);
+	fd = open(filename, 0_RDWR | 0_CREAT | 0_TRUNC, 0600);
 	if (fd < 0)
 		return (-1);
 
-	for (len = 0; text_content[len] != '\0'; len++)
+	for (len = 0; text_content && *(text_content + len); len++)
 		;
 
 	fdwrite = write(fd, text_content, len);
@@ -33,5 +33,4 @@ int create_file(const char *filename, char *text_content)
 	if (fdwrite < 0)
 		return (-1);
 	return (1);
-
 }

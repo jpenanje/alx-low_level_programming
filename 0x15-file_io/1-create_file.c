c80d9d9d19e527b922667e33706b4eb0cfd07f75
jpenanje <jpenanje@gmail.com>
2022-05-05 17:33:46 +0000

    create file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,37 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/uio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * create_file - create a file
+ * @filename: filename
+ * @text_content: file content
+ *
+ * Return: 1 on success, -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd, fdwrite, len;
+	
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, 0_CREAT | 0_RDWR | 0_TRUNC, 0600);
+	if (fd < 0)
+		return (-1);
+
+	for (len = 0; text_content[len] != '\0'; len++)
+		;
+
+	fdwrite = write(fd, text_content, len);
+	close(fd);
+	if (fdwrite < 0)
+		return (-1);
+	return (1);
+
+}

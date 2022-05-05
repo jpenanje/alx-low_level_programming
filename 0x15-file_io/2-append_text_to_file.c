425a20aeaf0ab2b3a12bdad047f528a479fc1134
jpenanje <99893508+jpenanje@users.noreply.github.com>
2022-05-05 18:52:40 +0100

    Update 2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -15,7 +15,7 @@ int append_text_to_file(const char *filename, char *text_content)
 {
 	int fdo, fdw, len = 0;
 
-	if (filename == NULL)						
+	if (filename == NULL)				
 		return (-1);
 
 	fdo = open(filename, O_RDWR | O_APPEND);
@@ -23,14 +23,14 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 	if (text_content == NULL)
 	{
-		close(fdo);							
-		return (1);							
+		close(fdo);					
+		return (1);						
 	}
 	while (*(text_content + len))
 		len++;
 	fdw = write(fdo, text_content, len);
 	close(fdo);
-	if (fdw < 0)								
+	if (fdw < 0)							
 		return (-1);
 	return (1);
 }

2eb9c1e00beb2d2f6d8d407d477e91c6e4598d00
jpenanje <99893508+jpenanje@users.noreply.github.com>
2022-05-05 18:12:59 +0100

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,47 +1,49 @@
-#include "main.h"
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <sys/uio.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <stdlib.h>
+#include "main.h"
 
 /**
- * read_textfile - read and print a text file to POSIX
- * @filename: file name
- * @letters: number of letters
- *
- * Return: number of letters to read and print
+ * read_textfile - a function that reads a text file and prints
+ * to the POSIX STDOUT
+ * @filename: the filename to open
+ * @letters: the number of letters to read and print
+ * Return: the number of letters read and prints, or 0 on failure
  */
-
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-        ssize_t	nread, nprnt;
-	char *buf;
+	int fdo, fdr, fdw;
+	char *temp;
 
 	if (filename == NULL)
 		return (0);
 
-	buf = malloc(sizeof(char) * (letters));
-	if (buf == NULL)
+	temp = malloc(sizeof(char) * letters);
+	if (temp == NULL)
 		return (0);
 
-	fd = open(filename, 0_RDONLY);
-	if (fd == -1)
+	fdo = open(filename, O_RDONLY);
+	if (fdo < 0)
+	{
+		free(temp);
 		return (0);
+	}
 
-	nread = read(fd, buf, letters);
-	if (nread < 0)
+	fdr = read(fdo, temp, letters);
+	if (fdr < 0)
+	{
+		free(temp);
 		return (0);
+	}
 
-	nprnt = write(STDOUT_FILENO, buf, nread);
+	fdw = write(STDOUT_FILENO, temp, fdr);
+	free(temp);
+	close(fdo);
 
-	free(buf);
-	close(fd);
-
-	if (nprnt < 0)
+	if (fdw < 0)
 		return (0);
-	return (nprnt);
+	return ((ssize_t)fdw);
 }
-
